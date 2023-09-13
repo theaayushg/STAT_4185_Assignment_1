32409f751323461c25b7d055a9ffb2f7f807ca7c
theaayushg <60559949+theaayushg@users.noreply.github.com>
2023-09-13 11:22:59 -0400

    Update decrypt_message_two.py

--- decrypt_message_two.py
+++ decrypt_message_two.py
@@ -4,4 +4,18 @@ encrypted_message = encrypted_file.readline()
 
 encrypted_file.close()
 
-# Write Code Here
+# Create an empty string to store the decrypted message
+decrypted_message = ""
+
+start = 0
+end = len(encrypted_message) - 2
+while start <= end:
+    decrypted_message += encrypted_message[start]
+    if start != end:
+        decrypted_message += encrypted_message[end]
+    start += 2
+    end -= 2
+decrypted_message += encrypted_message[-3]
+decrypted_message += encrypted_message[-1]
+
+print(decrypted_message)
